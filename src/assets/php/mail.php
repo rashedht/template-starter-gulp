60790dabc389e2ef5c93de2b6f5801aeac4177f6
rashedht <rashed@hastechit.com>
2019-03-02 12:30:56 +0600

    update

--- src/assets/php/mail.php
+++ src/assets/php/mail.php
@@ -3,14 +3,13 @@
     // Only process POST reqeusts.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form fields and remove whitespace.
-        $first_name = strip_tags(trim($_POST["customerName"]));
-		$first_name = str_replace(array("\r","\n"),array(" "," "),$first_name);
-        $email = filter_var(trim($_POST["customerEmail"]), FILTER_SANITIZE_EMAIL);
-        $subject = trim($_POST["contactSubject"]);
-        $message = trim($_POST["contactMessage"]);
+        $name = strip_tags(trim($_POST["con_name"]));
+				$name = str_replace(array("\r","\n"),array(" "," "),$name);
+        $email = filter_var(trim($_POST["con_email"]), FILTER_SANITIZE_EMAIL);
+        $message = trim($_POST["con_message"]);
 
         // Check that data was sent to the mailer.
-        if ( empty($first_name) OR empty($subject) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             // Set a 400 (bad request) response code and exit.
             http_response_code(400);
             echo "Please complete the form and try again.";
@@ -18,20 +17,18 @@
         }
 
         // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
-        $recipient = "admin@devitems.com";
+        $recipient = "test@hasthemes.com";
 
         // Set the email subject.
-        $subject = "$subject";
+        $subject = "Demo - Mail From $name";
 
         // Build the email content.
-        $email_content = "Name: $first_name\n";
+        $email_content = "Name: $name\n";
         $email_content .= "Email: $email\n\n";
-        $email_content .= "Subject: $subject\n\n";
         $email_content .= "Message:\n$message\n";
 
         // Build the email headers.
-        $email_headers = "From: $first_name <$email>";
+        $email_headers = "From: $name <$email>";
 
         // Send the email.
         if (mail($recipient, $subject, $email_content, $email_headers)) {
